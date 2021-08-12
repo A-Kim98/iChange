81bede94ca8f30a309d9c0617aa09d33cc329897
A-Kim98 <alicekimmy2020@gmail.com>
2021-08-12 18:42:51 -0400

    Created swift files

--- iChange/iChange/ViewController.swift
+++ iChange/iChange/ViewController.swift
@@ -5,20 +5,13 @@
 //  Created by Alice Kim on 2021-08-12.
 //
 
-import Cocoa
+import UIKit
 
-class ViewController: NSViewController {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
     }
 
 
