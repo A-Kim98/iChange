ef096497908cf4033bced24f0bf00cbbb298de8f
A-Kim98 <alicekimmy2020@gmail.com>
2021-08-12 18:33:49 -0400

    Creates Swift files

---
+++ iChange/iChange/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  iChange
+//
+//  Created by Alice Kim on 2021-08-12.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
