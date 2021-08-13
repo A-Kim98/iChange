5e012b069db9bb31c96852724de55364c4ccd932
A-Kim98 <alicekimmy2020@gmail.com>
2021-08-12 22:38:49 -0400

    added every needed items in the HomeView

---
+++ iChange/iChange/HomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HomeViewController.swift
+//  iChange
+//
+//  Created by Alice Kim on 2021-08-12.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        // set the background image
+        
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "iChangeBg")! )
+    }
+
+
+}
+
